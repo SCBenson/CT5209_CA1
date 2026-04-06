0e87fabe430c5dc325ed2571a76092195b647bc7
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2026-04-06 18:38:03 -0500

    Initial Spring Boot project setup with WAR packaging.

---
+++ src/main/java/com/ct5209/seanspetitions/SeanspetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.ct5209.seanspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeanspetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeanspetitionsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ct5209/seanspetitions/ServletInitializer.java
@@ -0,0 +1,11 @@
+package com.ct5209.seanspetitions;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(SeanspetitionsApplication.class);
+    }
+}
